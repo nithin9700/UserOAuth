42af36952a0119d9e0e596fe07f0aead895a7cf4
nithin@9700 <nithinkorukonda132@gmail.com>
2024-05-15 21:07:08 +0530

    security

--- src/main/java/dev/Nithin/OAuthUserSignIn/entity/BaseModel.java
+++ src/main/java/dev/Nithin/OAuthUserSignIn/entity/BaseModel.java
@@ -7,6 +7,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 import org.springframework.data.annotation.CreatedDate;
 
@@ -20,7 +21,7 @@ public abstract class BaseModel{
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
-    @CreatedDate
+    @CreationTimestamp
     private Instant createdAt;
     @UpdateTimestamp
     private Instant updatedAt;
